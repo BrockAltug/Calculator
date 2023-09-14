a3ce5e776d99a065744c0f2ccad166e21cfeb8ce
BrockAltug <brock.altug99@gmail.com>
2023-09-14 04:04:59 -0700

    created routes for calculator and moved index

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Define a route for the index page
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Define a route for the calculator page
+@app.route('/calculator')
+def calculator():
+    return render_template('calculator.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
